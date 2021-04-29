8969a510feaa8d8e56e2e9667fc47f4579a8c6cd
Fordley Hagedorn <fordleyh2900@gmail.com>
2021-04-29 12:33:48 -0500

    added more functionality to the register an restaurant controllers

--- HowdyFresh/HowdyFresh/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ HowdyFresh/HowdyFresh/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
@@ -115,6 +116,7 @@ namespace HowdyFresh.Areas.Identity.Pages.Account
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
+                    var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
                 }
                 foreach (var error in result.Errors)
                 {
